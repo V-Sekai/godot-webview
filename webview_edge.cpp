97b460853c8e65859d893378fc435d6eb6cfd7fb
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2022-03-15 19:58:04 -0700

    Show in the editor.

--- webview_edge.cpp
+++ webview_edge.cpp
@@ -235,14 +235,13 @@ WebViewOverlay::~WebViewOverlay() {
 void WebViewOverlay::_notification(int p_what) {
 	switch (p_what) {
 		case NOTIFICATION_ENTER_TREE: {
-			if (!Engine::get_singleton()->is_editor_hint() && (err_status == 0)) {
+			if (err_status == 0) {
 				set_process_internal(
-						true); // Wait for window to init, do not init in editor.
+						true); // Wait for window to init.
 			}
 		} break;
 		case NOTIFICATION_INTERNAL_PROCESS: {
-			if (!Engine::get_singleton()->is_editor_hint() && (data->view == nullptr) &&
-					(err_status == 0)) {
+			if ((data->view == nullptr) && (err_status == 0)) {
 				HWND hwnd =
 						(HWND)DisplayServer::get_singleton()->window_get_native_handle(DisplayServer::WINDOW_HANDLE, DisplayServer::WINDOW_HANDLE);
 				if (hwnd != nullptr) {
@@ -254,7 +253,7 @@ void WebViewOverlay::_notification(int p_what) {
 					ctrl_err_status = -1;
 				}
 			}
-			if (!Engine::get_singleton()->is_editor_hint() && (data->view != nullptr) &&
+			if ((data->view != nullptr) &&
 					(data->view->is_ready) && (ctrl_err_status == -1) &&
 					(err_status == 0)) {
 				float sc = DisplayServer::get_singleton()->screen_get_max_scale();
